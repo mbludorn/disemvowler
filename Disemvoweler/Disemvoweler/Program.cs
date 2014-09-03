bbce9432c53b6bc550999a6bf86f19f0d9e730ca
SeedPaths-User <lessdismissive@email.com>
2014-09-03 11:39:37 -0600

    disemvowler

---
+++ Disemvoweler/Disemvoweler/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disemvoweler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //call  the funtion
+            Disemvoweler("TWO DRUMS AND A CYMBAL FALL OFF A CLIFF");
+
+            Console.ReadKey();
+        }
+
+        static void Disemvoweler(string input)
+        {
+            string vowels = string.Empty;
+            string output = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                //check if the letter is a vowel or not
+                string letter = input[i].ToString();
+                if ("AEIOU".Contains(letter))
+
+                    vowels = vowels + letter;
+
+                else if (" ".Contains(letter))
+                {
+                    continue;
+                }
+                else
+                    {
+                    output = output + letter;
+                    }
+                }
+            Console.WriteLine(output);
+            Console.WriteLine(vowels);
+            }
+        }
+    }
